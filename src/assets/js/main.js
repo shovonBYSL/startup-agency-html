56e36ef4733269bebc60923b4da875b6d4211553
shovonBYSL <kawser.shovon@intelli.global>
2023-04-13 14:29:20 +0600

    accordion updated

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -84,3 +84,23 @@ $(".closeModal, .modalMain").on("click", () => {
   $(".modal").removeClass("!visible");
   document.body.style.overflow = "";
 });
+
+// accordion
+$(".toggle").click(function (e) {
+  e.preventDefault();
+
+  let $this = $(this);
+
+  if ($this.next().hasClass("show")) {
+    $this.next().removeClass("show");
+    $this.next().slideUp(350);
+  } else {
+    $this.parent().parent().find("li .inner").removeClass("show");
+    $this.parent().parent().find("li .inner").slideUp(350);
+    $this.next().toggleClass("show");
+    $this.next().slideToggle(350);
+  }
+
+  $this.parent().parent().find(".toggle").removeClass("spin");
+  $this.addClass("spin");
+});
