b239f0521aea89aa5549fa4f5166104a3126262b
shovonBYSL <kawser.shovon@intelli.global>
2023-04-18 10:36:28 +0600

    updated

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -39,7 +39,6 @@ var swiper = new Swiper(".swiper1", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  centeredSlides: true,
   loopFillGroupWithBlank: true,
 });
 
@@ -98,5 +97,35 @@ var swiper = new Swiper(".swiper3", {
     delay: 3500,
     disableOnInteraction: false,
   },
+  loopFillGroupWithBlank: true,
+});
+
+// centered slider
+var swiper = new Swiper(".swiper4", {
+  slidesPerView: "auto",
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  loop: true,
+  speed: 1500,
+  spaceBetween: 20,
+  grabCursor: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  centeredSlides: true,
   loopFillGroupWithBlank: true,
 });
